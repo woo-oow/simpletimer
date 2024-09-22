f0579d49b5ae96d204a9bca6218a611ca7edd6ba
Wow <160201362+woo-oow@users.noreply.github.com>
2024-09-22 10:04:47 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
+let timer;
+let seconds = 0;
 
+function updateTime() {
+    document.getElementById('timer').innerText = seconds;
+    seconds++;
+}
+
+function startTimer() {
+    timer = setInterval(updateTime, 1000);
+}
+
+function stopTimer() {
+    clearInterval(timer);
+}
+
+function resetTimer() {
+    stopTimer();
+    seconds = 0;
+    document.getElementById('timer').innerText = seconds;
+}
